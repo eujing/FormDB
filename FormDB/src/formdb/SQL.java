612f85ad139f9743bedce25cd0add0271b73d423
Lee Chang Yoon <cyoon47@hotmail.com>
2014-01-22 20:17:53 +0800

    Minor changes to Window.java to contain getPath() Added SQL CreateTable command generator

---
+++ FormDB/src/formdb/SQL.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formdb;
+
+/**
+ *
+ * @author LEE
+ */
+public class SQL {
+    
+    PDFFieldReader reader = new PDFFieldReader(Window.getPath());
+    DataField[] data = reader.getDataFields();
+    String command = "CREATE TABLE forms(Number INTEGER NOT NULL AUTO_INCREMENT, ";
+    
+    public String createCommand(){
+        boolean temp = false;
+        
+        for(int i = 0; i < data.length; i++){
+            if(data[i].isBoolean()){
+                if(data[i].getFieldValue().equalsIgnoreCase("yes"))
+                    temp = true;
+                else
+                    temp = false;
+                command += data[i].getFieldName() + " " + temp + ", ";
+            }
+            else{
+                command += data[i].getFieldName() + " VARCHAR(255), ";
+            }
+        }
+        
+        command += "PRIMARY KEY (Number)";
+        
+        return command;
+    }
+    
+    
+}
